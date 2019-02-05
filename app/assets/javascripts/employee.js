6c0f20cfdca4db47feae48d16fc5d372a66457b8
roitblatari <roitblatari@gmail.com>
2019-02-04 21:45:38 -0500

    employeeNewForm employeeLoginForm

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -1,6 +1,35 @@
 $(function () {
   console.log("app/assets/javascripts/employee.js")
-
-
-
 })
+
+const employeeNewForm = () => {
+  $('span#new_employee').on("click", function (e) {
+    e.preventDefault()
+    debugger
+
+    $.ajax({
+      url: 'http://0.0.0.0:3000/employees/new',
+      method: 'GET',
+      dataType: 'html'
+    }).success(function (response) {
+      $('div#app-div-id').html(response)
+
+    })
+  })
+}
+
+const employeeLoginForm = () => {
+  $('a#employee-login').on("click", function (e) {
+    e.preventDefault()
+    // debugger
+    
+    $.ajax({
+      url: 'http://0.0.0.0:3000/signin_employee',
+      method: 'GET',
+      dataType: 'html'
+    }).success(function (response) {
+      $('div#app-div-id').html(response)
+      
+    })
+  })
+ } 
