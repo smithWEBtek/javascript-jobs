4d6e718866c63f68e9087f39e373f572f49de2a4
roitblatari <roitblatari@gmail.com>
2019-02-06 12:00:37 -0500

    Moved everything to index.js

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -1,53 +1,50 @@
-$(function () {
-  console.log("app/assets/javascripts/employee.js")
-})
+// $(function () {
+//   console.log("app/assets/javascripts/employee.js")
+// })
 
-const employeeNewForm = () => {
-  $('span#new_employee').on("click", function (e) {
-    e.preventDefault()
-    $.ajax({
-      url: 'http://0.0.0.0:3000/employees/new',
-      method: 'GET',
-      dataType: 'html'
-    }).success(function (response) {
-      $('div#app-div-id').html(response)
+// const employeeNewForm = () => {
+//   $('span#new_employee').on("click", function (e) {
+//     e.preventDefault()
+//     $.ajax({
+//       url: 'http://0.0.0.0:3000/employees/new',
+//       method: 'GET',
+//       dataType: 'html'
+//     }).success(function (response) {
+//       $('div#app-div-id').html(response)
+//       submitEmployeeNewForm();
+//     })
+//   })
+// }
 
-    })
-  })
-}
+// const submitEmployeeNewForm = () => {
+//   $('#new_employee_form').on("submit", function(e) {
 
-const submitEmployeeNewForm = () => {
-  $('#new_employee_form').on("click", function(e) {
+//     e.preventDefault()
+//     debugger;
+//     $.ajax({
+//       type: "POST",
+//       dataType: $(this).serialize(),
+//       url: this.action
+//     }).success(function (response) {
+//       debugger
+//       alert("you submited the form")
+//       $('div#app-div-id').html(response)
+
+//     })
+//   })
+// }
+
+// const employeeLoginForm = () => {
+//   $('a#employee-login').on("click", function (e) {
+//     e.preventDefault()
+//     // debugger
     
-    e.preventDefault()
-  
-    $.ajax({
-      type: "POST",
-      dataType: $(this).serialize(),
-      url: this.action
-    }).success(function (response) {
-      debugger
-      alert("you submited the form")
-      $('div#app-div-id').html(response)
-
-    })
-   
-    
-  })
-}
-
-const employeeLoginForm = () => {
-  $('a#employee-login').on("click", function (e) {
-    e.preventDefault()
-    // debugger
-    
-    $.ajax({
-      url: 'http://0.0.0.0:3000/signin_employee',
-      method: 'GET',
-      dataType: 'html'
-    }).success(function (response) {
-      $('div#app-div-id').html(response)
-      
-    })
-  })
- } 
+//     $.ajax({
+//       url: 'http://0.0.0.0:3000/signin_employee',
+//       method: 'GET',
+//       dataType: 'html'
+//     }).success(function (response) {
+//       $('div#app-div-id').html(response);
+//     })
+//   })
+//  } 
